edc663c098f673ac62abb9ce46eacab3afd25521
Rodrigo Avelar <rodrigo.avelar.2003@gmail.com>
2026-04-28 00:37:21 +0000

    (27/04) Código completo do back-end, com correções

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -17,8 +17,19 @@ app.use('/users', usersRouter)
 
 /******************** ROTAS ********************/
 
-import categoriasRouter from '../src/routes/categorias.js'
+import categoriasRouter from './routes/categorias.js'
 app.use('/categorias', categoriasRouter)
 
+import clientesRouter from './routes/clientes.js'
+app.use('/clientes', clientesRouter)
+
+import fornecedoresRouter from './routes/fornecedores.js'
+app.use('/fornecedores', fornecedoresRouter)
+
+import pedidosRouter from './routes/pedidos.js'
+app.use('/pedidos', pedidosRouter)
+
+import produtosRouter from './routes/produtos.js'
+app.use('/produtos', produtosRouter)
 
 export default app
